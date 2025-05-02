4c1e37555af3ef13ea18e6dcffbe8f086c72c765
kushagra <kushagragangwal@gmail.com>
2025-05-02 10:47:40 +0530

    initialized api server with base controller

--- cmd/users-api/main.go
+++ cmd/users-api/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-func main(){
-	fmt.Println("Hello! GO")
+	"github.com/Kushhu/users.api.git/internal/config"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to users api"))
+	})
+
+	server := http.Server{
+		Addr:    cfg.Address,
+		Handler: router,
+	}
+
+	fmt.Println("server initiated...")
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal("failed to start server")
+	}
 }
